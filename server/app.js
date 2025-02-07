0fca3a529c51c1084590aaa0821abb897485f756
nitishpratap18 <nitish@itinker.io>
2025-02-07 11:22:30 +0530

    added source origin

--- server/app.js
+++ server/app.js
@@ -6,15 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-const allowedOrigins = [
-    "https://punjab-soft-leai8y5bs-nitishprataps-projects.vercel.app"
-];
-app.use(
-    cors({
-        origin: allowedOrigins,
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        credentials: true
-    }))
+app.use(cors())
 
 handleRequest(app);
 
